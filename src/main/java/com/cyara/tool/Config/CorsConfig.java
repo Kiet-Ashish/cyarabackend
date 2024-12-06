0fd17dd4c1b714346facdb1d4df852d336a166cc
Kiet-Ashish <Ashish9758592845@gmail.com>
2024-12-07 00:25:29 +0530

    update cors error

--- src/main/java/com/cyara/tool/Config/CorsConfig.java
+++ src/main/java/com/cyara/tool/Config/CorsConfig.java
@@ -6,12 +6,14 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.Arrays;
+
 @Configuration
 public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOriginPattern("http://localhost:3000"); // Allow all origins
+        corsConfiguration.setAllowedOriginPatterns(Arrays.asList("https://www.cyara.in", "https://cyara.in")); // Allow all origins
         corsConfiguration.addAllowedMethod("*");       // Allow all HTTP methods
         corsConfiguration.addAllowedHeader("*");       // Allow all headers
         corsConfiguration.setAllowCredentials(true);   // Allow credentials
